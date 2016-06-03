d41f1b2b3277c9bcb6d3855e38659ab3491cb28f
BertrandHustle <scott.greenberg926@gmail.com>
2016-06-03 12:00:56 -0400

    displays search history

--- src/clojure_crud/core.clj
+++ src/clojure_crud/core.clj
@@ -5,12 +5,28 @@
             [ring.adapter.jetty :as ring]
             [hiccup.core :as hic]))
 
-;(defonce server (atom nil))
+(defonce server (atom nil))
 
-(defn -main []
+;lets user enter search query
+(defn query []
   (println "enter a search query")
   (let [query (read-line)
         file-text (pr-str query)]
     (spit (str "search-history.edn") file-text)
     (println query)))
 
+;webroot, displays search history
+(comp/defroutes root
+                (comp/GET "/" []
+                  (let [history (slurp "search-history.edn")
+                        ](hic/html[:html history]))))
+;search route
+(comp/defroutes search
+                (comp/GET "/search" []
+                  (query)))
+
+(defn -main []
+  ;@ points to defonce binding
+  (when @server
+    (.stop @server))
+  (reset! server (ring/run-jetty root {:port 3000 :join? false})))
